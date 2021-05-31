8f5f1f9d68a42d9d3fcafb49261d4d3c042d68dd
charliejuc <charliejuc@gmail.com>
2021-05-31 11:09:38 +0200

    tFilter

--- src/Transducers.ts
+++ src/Transducers.ts
@@ -1,11 +1,19 @@
 import R from 'ramda'
 
 type Reducer<Acc, Current> = (acc: Acc, current: Current) => Acc
+// (transform) => (reducer) => (acc, current) => reducer(acc, transform(current))
 const tMap =
     <Value, TransformResult, Acc>(transform: (value: Value) => TransformResult) =>
     (reducer: Reducer<Acc, TransformResult>) =>
     (acc: Acc, current: Value): Acc =>
         reducer(acc, transform(current))
+
+// (condition) => (reducer) => (acc, current) => condition(current) ? reducer(acc, current) : acc
+const tFilter =
+    <Value, Acc>(condition: (value: Value) => boolean) =>
+    (reducer: Reducer<Acc, Value>) =>
+    (acc: Acc, current: Value): Acc =>
+        condition(current) ? reducer(acc, current) : acc
 
 const users = [
     {
@@ -24,16 +32,18 @@ const users = [
 
 const getUsersFirstNumber = <Acc>() =>
     tMap<typeof users[number], string, Acc>((user) => user.phones[0])
-const addComma = <Acc>() => tMap<string, string, Acc>((phone) => phone + ',')
+const spanishNumbers = <Acc>() => tFilter<string, Acc>((phone) => phone.startsWith('+34'))
 
 const getUsersFirstNumberAsString = getUsersFirstNumber<string>()
-const addCommaAsString = addComma<string>()
+const spanishNumbersAsString = spanishNumbers<string>()
 
-const composed = R.compose(getUsersFirstNumberAsString, addCommaAsString)
+const composed = R.compose(getUsersFirstNumberAsString, spanishNumbersAsString)
 
 console.log(
-    users.reduce(
-        composed((acc, current) => acc + current),
-        ''
-    )
+    users
+        .reduce(
+            composed((acc, current) => `${acc},${current}`),
+            ''
+        )
+        .slice(1)
 )
